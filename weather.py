2fb55fd29911dd3075467c9c9335f4ddb5d28496
h-karim <karimh96@hotmail.com>
2019-06-28 05:28:18 -0400

    icon display Icon will now display next to temperature indicating current weather

--- weather.py
+++ weather.py
@@ -1,6 +1,25 @@
 import requests, json, time
 from config import api_key 
 
+icons_mapping = {"01d":"\uf00d",
+        "01n":"\uf02e",
+        "02d":"\uf002",
+        "02n": "\uf086",
+        "03n":"\uf041", 
+        "03d":"\uf041",
+        "04n":"\uf041",
+        "04d": "\uf041",
+        "09n":"\uf01a",
+        "09d":"\uf01a",
+        "10d":"\uf008",
+        "10n":"\uf028",
+        "11d":"\uf010",
+        "11n":"\uf03b",
+        "13d":"\uf00a",
+        "13n":"\uf02a",
+        "50d":"\uf003",
+        "50n":"\uf04a"}
+
 def get_location():
     while True:
         try:
@@ -17,11 +36,12 @@ def get_weather(lat, lon):
     lat, lon = str(lat), str(lon)
     url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID="+api_key
     data = requests.get(url).json()
-    weather = data["weather"]
-    main = weather[0]["main"] 
+    weather_data = data["weather"]
     temp = data["main"]["temp"] -272.15
-    print( str(format(temp, ".1f"))+"°C")
-    return temp
+    icon_code = weather_data[0]["icon"]
+    icon = icons_mapping[icon_code]
+    return [icon, str(format(temp, ".1f")+"°C")]
 
 latitude, longitude = get_location()
-get_weather(latitude, longitude)
+weather = get_weather(latitude, longitude)
+print(weather[0]+weather[1], end="")
